ba1a4ff5422bf8ce2eb5e204b9dcaf1e51139709
Marko Radosavljević <marko@radosavljevic.dev>
2022-07-31 23:58:04 +0200

    Add notes to the interface javadocs

--- Contest/Assignment/src/org/togetherjava/event/elevator/elevators/FloorPanelSystem.java
+++ Contest/Assignment/src/org/togetherjava/event/elevator/elevators/FloorPanelSystem.java
@@ -6,9 +6,12 @@ package org.togetherjava.event.elevator.elevators;
 public interface FloorPanelSystem {
     /**
      * Requests an elevator to move to the given floor to pick up a human.
-     * @param atFloor the floor to pick up the human at, must be within the range served by the system
+     *
+     * @param atFloor                the floor to pick up the human at, must be within the range served by the system
      * @param desiredTravelDirection the direction the human wants to travel into,
      *                               can be used for determination of the best elevator
+     * @apiNote This represents a human standing in the corridor, pressing a button on the wall,
+     * requesting that an elevator comes to pick them up for travel into the given direction.
      */
     void requestElevator(int atFloor, TravelDirection desiredTravelDirection);
 }
